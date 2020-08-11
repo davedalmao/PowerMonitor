ad48aff63eeb0488ed41be83d8b9922409b294f7
John Dave Dalmao <jhav3dalx@gmail.com>
2020-08-11 17:36:39 +0800

    Initial Commit

---
+++ Power Status/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Power_Status
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BatteryIndicator = new System.Windows.Forms.ProgressBar();
+            this.BatteryLife = new System.Windows.Forms.Label();
+            this.BatteryStatus = new System.Windows.Forms.Label();
+            this.RefreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.PowerStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BatteryIndicator
+            // 
+            this.BatteryIndicator.Location = new System.Drawing.Point(70, 42);
+            this.BatteryIndicator.Name = "BatteryIndicator";
+            this.BatteryIndicator.Size = new System.Drawing.Size(270, 30);
+            this.BatteryIndicator.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.BatteryIndicator.TabIndex = 3;
+            // 
+            // BatteryLife
+            // 
+            this.BatteryLife.AutoSize = true;
+            this.BatteryLife.Location = new System.Drawing.Point(67, 85);
+            this.BatteryLife.Name = "BatteryLife";
+            this.BatteryLife.Size = new System.Drawing.Size(112, 13);
+            this.BatteryLife.TabIndex = 4;
+            this.BatteryLife.Text = "Charge Remaining:   ?";
+            // 
+            // BatteryStatus
+            // 
+            this.BatteryStatus.AutoSize = true;
+            this.BatteryStatus.Location = new System.Drawing.Point(67, 110);
+            this.BatteryStatus.Name = "BatteryStatus";
+            this.BatteryStatus.Size = new System.Drawing.Size(94, 13);
+            this.BatteryStatus.TabIndex = 5;
+            this.BatteryStatus.Text = "Battery Status:    ?";
+            // 
+            // RefreshTimer
+            // 
+            this.RefreshTimer.Interval = 1000;
+            this.RefreshTimer.Tick += new System.EventHandler(this.RefreshTimer_Tick);
+            // 
+            // PowerStatus
+            // 
+            this.PowerStatus.AutoSize = true;
+            this.PowerStatus.Location = new System.Drawing.Point(67, 16);
+            this.PowerStatus.Name = "PowerStatus";
+            this.PowerStatus.Size = new System.Drawing.Size(56, 13);
+            this.PowerStatus.TabIndex = 6;
+            this.PowerStatus.Text = "DEFAULT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 151);
+            this.Controls.Add(this.PowerStatus);
+            this.Controls.Add(this.BatteryStatus);
+            this.Controls.Add(this.BatteryLife);
+            this.Controls.Add(this.BatteryIndicator);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar BatteryIndicator;
+        private System.Windows.Forms.Label BatteryLife;
+        private System.Windows.Forms.Label BatteryStatus;
+        private System.Windows.Forms.Timer RefreshTimer;
+        private System.Windows.Forms.Label PowerStatus;
+    }
+}
+
---
+++ Power Status/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Power_Status
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshStatus();
+            RefreshTimer.Enabled = true;            
+        }
+
+        private void RefreshTimer_Tick(object sender, EventArgs e)
+        {
+            RefreshStatus();
+        }
+        
+        PowerStatus power = SystemInformation.PowerStatus;
+
+        private void RefreshStatus()
+        {
+            CheckChargeState();
+            VisualBatteryHealth();
+
+             //Battery Percent
+            BatteryLife.Text = "Charge Remaining:   " + power.BatteryLifePercent.ToString("P0");
+            BatteryStatus.Text = power.BatteryChargeStatus.ToString();
+
+            //Check Battery Percentage
+            int a = (int)(power.BatteryLifePercent * 100);
+            if (a <= 25)
+            {
+                this.Show();
+            }
+        }
+
+        private void CheckChargeState()
+        {
+            switch (power.PowerLineStatus)
+            {
+                case PowerLineStatus.Online:
+                    //MainsPower.Checked = true; 
+                    PowerStatus.Text = "CHARGING";
+                    break;
+
+                case PowerLineStatus.Offline:
+                    //MainsPower.Checked = false;
+                    PowerStatus.Text = "UNPLUGGED";
+                    break;
+
+                case PowerLineStatus.Unknown:
+                    //MainsPower.CheckState = CheckState.Indeterminate;
+                    PowerStatus.Text = "INDETERMINATE";
+                    break;
+            }
+        }
+
+        private void VisualBatteryHealth()
+        {
+            int powerPercent = (int)(power.BatteryLifePercent * 100);
+            if (powerPercent <= 100)
+            {
+                BatteryIndicator.Value = powerPercent;
+            }
+
+            else
+            {
+                BatteryIndicator.Value = 0;
+            }
+        }
+
+        public void a()
+        {
+           
+        }
+    }
+}
+
+//ADD
+//modify time interval update in the ui
