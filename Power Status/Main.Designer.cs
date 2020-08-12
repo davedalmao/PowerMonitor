a0001963272f523746ec1cd8ab33d78706995547
John Dave Dalmao <jhav3dalx@gmail.com>
2020-08-12 15:10:54 +0800

    Added Colors for the UI and Added 2 Timers

---
+++ Power Status/Main.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Power_Status {
+    partial class Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.BatteryIndicator = new System.Windows.Forms.ProgressBar();
+            this.BatteryLife = new System.Windows.Forms.Label();
+            this.BatteryTime = new System.Windows.Forms.Label();
+            this.RefreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.PowerStatus = new System.Windows.Forms.Label();
+            this.Warning = new System.Windows.Forms.Label();
+            this.CloseIcon = new System.Windows.Forms.PictureBox();
+            this.MinimizeIcon = new System.Windows.Forms.PictureBox();
+            this.ForLow = new System.Windows.Forms.Timer(this.components);
+            this.ForHigh = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.CloseIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinimizeIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BatteryIndicator
+            // 
+            this.BatteryIndicator.Location = new System.Drawing.Point(49, 72);
+            this.BatteryIndicator.Name = "BatteryIndicator";
+            this.BatteryIndicator.Size = new System.Drawing.Size(355, 30);
+            this.BatteryIndicator.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.BatteryIndicator.TabIndex = 3;
+            // 
+            // BatteryLife
+            // 
+            this.BatteryLife.AutoSize = true;
+            this.BatteryLife.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BatteryLife.Location = new System.Drawing.Point(46, 115);
+            this.BatteryLife.Name = "BatteryLife";
+            this.BatteryLife.Size = new System.Drawing.Size(163, 17);
+            this.BatteryLife.TabIndex = 4;
+            this.BatteryLife.Text = "Charge Remaining:   ?";
+            // 
+            // BatteryTime
+            // 
+            this.BatteryTime.AutoSize = true;
+            this.BatteryTime.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BatteryTime.Location = new System.Drawing.Point(46, 140);
+            this.BatteryTime.Name = "BatteryTime";
+            this.BatteryTime.Size = new System.Drawing.Size(151, 17);
+            this.BatteryTime.TabIndex = 5;
+            this.BatteryTime.Text = "Time Remaining:    ?";
+            // 
+            // RefreshTimer
+            // 
+            this.RefreshTimer.Interval = 1000;
+            this.RefreshTimer.Tick += new System.EventHandler(this.RefreshTimer_Tick);
+            // 
+            // PowerStatus
+            // 
+            this.PowerStatus.AutoSize = true;
+            this.PowerStatus.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PowerStatus.Location = new System.Drawing.Point(46, 46);
+            this.PowerStatus.Name = "PowerStatus";
+            this.PowerStatus.Size = new System.Drawing.Size(73, 17);
+            this.PowerStatus.TabIndex = 6;
+            this.PowerStatus.Text = "DEFAULT";
+            // 
+            // Warning
+            // 
+            this.Warning.AutoSize = true;
+            this.Warning.Font = new System.Drawing.Font("Palatino Linotype", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Warning.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(107)))), ((int)(((byte)(34)))));
+            this.Warning.Location = new System.Drawing.Point(7, 36);
+            this.Warning.Name = "Warning";
+            this.Warning.Size = new System.Drawing.Size(440, 27);
+            this.Warning.TabIndex = 7;
+            this.Warning.Text = "BATTERY LOW, PLUG IN YOUR CHARGER!";
+            this.Warning.Visible = false;
+            // 
+            // CloseIcon
+            // 
+            this.CloseIcon.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseIcon.Image = global::Power_Status.Properties.Resources.Minus_UI;
+            this.CloseIcon.Location = new System.Drawing.Point(406, 9);
+            this.CloseIcon.Name = "CloseIcon";
+            this.CloseIcon.Size = new System.Drawing.Size(20, 19);
+            this.CloseIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CloseIcon.TabIndex = 10;
+            this.CloseIcon.TabStop = false;
+            this.CloseIcon.Click += new System.EventHandler(this.CloseIcon_Click);
+            // 
+            // MinimizeIcon
+            // 
+            this.MinimizeIcon.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MinimizeIcon.Image = ((System.Drawing.Image)(resources.GetObject("MinimizeIcon.Image")));
+            this.MinimizeIcon.Location = new System.Drawing.Point(380, 9);
+            this.MinimizeIcon.Name = "MinimizeIcon";
+            this.MinimizeIcon.Size = new System.Drawing.Size(20, 19);
+            this.MinimizeIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MinimizeIcon.TabIndex = 9;
+            this.MinimizeIcon.TabStop = false;
+            // 
+            // ForLow
+            // 
+            this.ForLow.Interval = 1000;
+            this.ForLow.Tick += new System.EventHandler(this.ForLow_Tick);
+            // 
+            // ForHigh
+            // 
+            this.ForHigh.Interval = 1000;
+            this.ForHigh.Tick += new System.EventHandler(this.ForHigh_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(35)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(455, 175);
+            this.Controls.Add(this.CloseIcon);
+            this.Controls.Add(this.MinimizeIcon);
+            this.Controls.Add(this.Warning);
+            this.Controls.Add(this.PowerStatus);
+            this.Controls.Add(this.BatteryTime);
+            this.Controls.Add(this.BatteryLife);
+            this.Controls.Add(this.BatteryIndicator);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Main_MouseUp);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.CloseIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinimizeIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar BatteryIndicator;
+        private System.Windows.Forms.Label BatteryLife;
+        private System.Windows.Forms.Label BatteryTime;
+        private System.Windows.Forms.Timer RefreshTimer;
+        private System.Windows.Forms.Label PowerStatus;
+        private System.Windows.Forms.Label Warning;
+        private System.Windows.Forms.PictureBox MinimizeIcon;
+        private System.Windows.Forms.PictureBox CloseIcon;
+        private System.Windows.Forms.Timer ForLow;
+        private System.Windows.Forms.Timer ForHigh;
+    }
+}
+
---
+++ Power Status/Main.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Power_Status {
+    /*public static class ModifyProgressBarColor {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
+        public static void SetState(this ProgressBar pBar, int state) {
+            SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
+        }
+    }*/
+
+    public partial class Main : Form {
+        PowerStatus power = SystemInformation.PowerStatus;
+        int percentNumber;
+        int mov;
+        int movX;
+        int movY;
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+
+        public Main() {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 18, 18));
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            RefreshStatus();
+            RefreshTimer.Enabled = true;
+        }
+
+        private void RefreshTimer_Tick(object sender, EventArgs e) {
+            RefreshStatus();
+        }
+
+        private void ForLow_Tick(object sender, EventArgs e) {
+            CheckpercentNumberLow();
+        }
+
+        private void ForHigh_Tick(object sender, EventArgs e) {
+            CheckpercentNumberFull();
+        }
+
+        private void Main_Resize(object sender, EventArgs e) {
+            if (this.WindowState == FormWindowState.Minimized) {
+                this.ShowInTaskbar = false;
+                this.Hide();
+            }
+        }
+
+        private void CloseIcon_Click(object sender, EventArgs e) {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Main_MouseDoubleClick(object sender, MouseEventArgs e) {
+            this.CenterToScreen();
+        }
+
+        private void Main_MouseDown(object sender, MouseEventArgs e) {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+
+        private void Main_MouseMove(object sender, MouseEventArgs e) {
+            if (mov == 1) {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+
+        private void Main_MouseUp(object sender, MouseEventArgs e) {
+            mov = 0;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void RefreshStatus() {
+            BatteryState();
+            VisualBatteryHealth();
+
+            //Battery Percent
+            BatteryLife.Text = "Charge Remaining:   " + power.BatteryLifePercent.ToString("P0");
+
+            ChargedState();
+
+            //Percent Number
+            percentNumber = (int)(power.BatteryLifePercent * 100);
+            BatteryTime.Text = percentNumber.ToString();
+        }
+
+        private void BatteryState() {
+            var status = SystemInformation.PowerStatus.BatteryChargeStatus;
+            if (status != BatteryChargeStatus.NoSystemBattery) {
+                var batteryStatus = status == 0 ? "NOT CHARGING" : status.ToString().ToUpper();
+                PowerStatus.Text = batteryStatus;
+                //BatteryStatus.Text = power.BatteryChargeStatus.ToString();
+            }
+        }
+
+        private void VisualBatteryHealth() {
+            percentNumber = (int)(power.BatteryLifePercent * 100);
+            if (percentNumber <= 100) {
+                BatteryIndicator.Value = percentNumber;
+            }
+
+            else {
+                BatteryIndicator.Value = 0;
+            }
+        }
+
+        private void ChargedState() {
+            try {
+                //Low Power State
+                if (percentNumber <= 25 && power.PowerLineStatus == PowerLineStatus.Offline) {
+                    ShowMain();
+                    Warning.Visible = true;
+                    ////////////////
+                    //Add sound here
+                    ////////////////
+                }
+
+                //98% state
+                else if (percentNumber >= 98 && power.PowerLineStatus == PowerLineStatus.Online) {
+                    ShowMain();
+                    ////////////////
+                    //Add sound here
+                    ////////////////
+                }
+
+                //100% State
+                else if (percentNumber == 100) {
+                    BatteryLife.Text = "Full Charged:   " + power.BatteryLifePercent.ToString("P0");
+                }
+
+                else {
+                    //BatteryIndicator.SetState(1); //Green
+                    //BatteryIndicator.SetState(2); //Red
+                    //BatteryIndicator.SetState(3); //Yellow
+
+                }
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+                Application.Exit();
+            }
+        }
+
+        private void CheckpercentNumberLow() {
+            try {
+                if (percentNumber <= 25 && power.PowerLineStatus == PowerLineStatus.Offline) {
+                    Warning.Visible = true;
+                    ForLow.Stop();
+                    ForHigh.Start();
+                    ////////////////
+                    //Add sound here
+                    ////////////////
+                }
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+                Application.Exit();
+            }
+        }
+
+        private void CheckpercentNumberFull() {
+            try {
+                if (percentNumber >= 92 && power.PowerLineStatus == PowerLineStatus.Online) {
+                    ForHigh.Stop();
+                    ForLow.Start();
+                    ////////////////
+                    //Add sound here
+                    ////////////////
+                }
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+                Application.Exit();
+            }
+        }
+
+        private void ShowMain() {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
+
+//ADD
+//modify time interval update in the ui
+
+
+//Check Battery Percentage
+//percentNumber = (int)(power.BatteryLifePercent * 100);
+
+
+
+//Implement this, change code above, and add 2 timers, pag base ra sa github
+/*         private void CheckpercentNumberLow() {
+            try {
+                if (percentNumber <= 25 && power.PowerLineStatus == PowerLineStatus.Offline) {
+                    ShowMain();
+                    Warning.Visible = true;
+                    ////////////////
+                    //Add sound here
+                    ////////////////
+                }
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+                Application.Exit();
+            }
+        }
+
+        private void CheckpercentNumberFull() {
+            try {
+                if (percentNumber >= 98 && power.PowerLineStatus == PowerLineStatus.Online) {
+                    ShowMain();
+                    ////////////////
+                    //Add sound here
+                    ////////////////
+                }
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+                Application.Exit();
+            }
+        }*/
